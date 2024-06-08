7194bf8ff23b1d4044a50ab37a259101c10ac5be
selena60635 <selena606352000@gmail.com>
2024-06-08 18:56:52 +0800

    fix: Edit root node name, fix the root node 'Enter' keydown event

--- client/src/MindMap.js
+++ client/src/MindMap.js
@@ -2,32 +2,48 @@ import React, { useState, useRef, useEffect } from "react";
 
 //根節點元件
 const RootNode = (props) => {
+  const [rootNodeName, setRootNodeName] = useState("根節點");
+  const [isEditing, setIsEditing] = useState(false);
   const inputDivRef = useRef(null);
+
   //當isEditing改變時，執行該useEffect。若處於編輯模式且inputDivRef不為空，則將焦點設置到輸入框上。
   useEffect(() => {
-    // console.log("inputDivRef.current:", inputDivRef.current);
-    if (props.isEditing && inputDivRef.current) {
-      console.log("inputDivRef.current:", inputDivRef.current);
+    if (isEditing && inputDivRef.current) {
       inputDivRef.current.focus();
     }
-  }, [props.isEditing]);
+  }, [isEditing]);
+
+  //開啟編輯模式
+  const editMode = () => {
+    setIsEditing(true);
+  };
+  //關閉編輯模式
+  const unEditMode = (e) => {
+    setRootNodeName(e.target.innerText);
+    setIsEditing(false);
+  };
 
   return (
-    <div className="rootnode" tabIndex="0" onDoubleClick={props.editMode}>
-      {props.isEditing ? (
+    <div className="rootnode" tabIndex="0" onDoubleClick={editMode}>
+      {isEditing ? (
         <div
           ref={inputDivRef}
           className="input-box"
           contentEditable="true"
           //加上這行才不會有Warning: A component is `contentEditable` and contains `children` managed by React....
           suppressContentEditableWarning="true"
-          onBlur={props.unEditMode}
-          onKeyDown={props.unEditMode}
+          onBlur={unEditMode}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              e.preventDefault();
+              unEditMode(e);
+            }
+          }}
         >
-          {props.rootNodeName}
+          {rootNodeName}
         </div>
       ) : (
-        <span>{props.rootNodeName}</span>
+        <span>{rootNodeName}</span>
       )}
     </div>
   );
@@ -44,32 +60,13 @@ const Node = (props) => {
 
 //心智圖組件
 const MindMap = () => {
-  const [rootNodeName, setRootNodeName] = useState("根節點");
-  const [isEditing, setIsEditing] = useState(false);
-
-  //開啟編輯模式
-  const editMode = () => {
-    setIsEditing(true);
-  };
-  //關閉編輯模式git
-  const unEditMode = (e) => {
-    if (e.key === "Enter") {
-      e.preventDefault();
-      setRootNodeName(e.target.innerText);
-    } else {
-      setRootNodeName(e.target.innerText);
-    }
-    setIsEditing(false);
-  };
+  const [nodes, setNodes] = useState([]);
 
   return (
-    <div className="">
-      <RootNode
-        editMode={editMode}
-        unEditMode={unEditMode}
-        isEditing={isEditing}
-        rootNodeName={rootNodeName}
-      />
+    <div className="d-flex align-items-center">
+      <RootNode />
+      <div className="nodes"></div>
+      <div className=""></div>
     </div>
   );
 };
