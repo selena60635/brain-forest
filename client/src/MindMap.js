ec29b527686527af552c18e10b87c241a48acace
selena60635 <selena606352000@gmail.com>
2024-06-23 17:58:51 +0800

    feat: Add delete node functionality

--- client/src/MindMap.js
+++ client/src/MindMap.js
@@ -1,45 +1,63 @@
-import React, { useState, useRef, useEffect, useLayoutEffect } from "react";
+import React, {
+  useState,
+  useRef,
+  useEffect,
+  useLayoutEffect,
+  useCallback,
+} from "react";
 import { v4 as uuidv4 } from "uuid";
 
-//根節點元件
-const RootNode = ({ addNode, rootRef, rootNode, setRootNode, rootNodeRef }) => {
-  const [isEditing, setIsEditing] = useState(false);
+//聚焦到inputbox並自動選取文本
+const selectText = (inputElement) => {
+  if (inputElement) {
+    inputElement.focus(); //將焦點設置到inputRef目前引用的Dom元素上
+    //選取整個文本
+    const range = document.createRange(); //創建一個新的Range物件，用來表示文本
+    range.selectNodeContents(inputElement); //加入inputRef目前引用的Dom元素的所有內容(包含textnode的文本)
+    const selection = window.getSelection(); //取得目前選擇的文本範圍(Selection物件)
+    selection.removeAllRanges(); //清除目前選擇的文本範圍
+    selection.addRange(range); //將包含目前選取文本內容的Range物件加入到目前selection中
+  }
+};
+
+// 根節點元件
+const RootNode = ({
+  isEditRoot,
+  setIsEditRoot,
+  rootRef,
+  rootNode,
+  setRootNode,
+  rootNodeRef,
+  isSelected,
+}) => {
   const inputRef = useRef(null);
 
-  //當isEditing改變時，執行該useEffect。若處於編輯模式且inputRef不為空，則將焦點設置到輸入框上。
+  //進入編輯模式後切換焦點
   useEffect(() => {
-    if (isEditing && inputRef.current) {
-      inputRef.current.focus();
+    if (isEditRoot && inputRef.current) {
+      selectText(inputRef.current);
     }
-  }, [isEditing]);
+  }, [isEditRoot]);
 
-  //開啟編輯模式
+  // 開啟編輯模式
   const editMode = () => {
-    setIsEditing(true);
+    setIsEditRoot(true);
   };
-  //關閉編輯模式
+
+  // 關閉編輯模式
   const unEditMode = (e) => {
-    setRootNode((prev) => ({ ...prev, name: e.target.innerText }));
-    setIsEditing(false);
-    if (rootRef.current) {
-      rootRef.current.focus(); // 關閉編輯模式後將焦點設置回根節點
-    }
+    setRootNode((prev) => ({ ...prev, name: e.target.innerText })); //將inputbox輸入的新名稱更新到rootNode狀態中
+    setIsEditRoot(false);
   };
 
   return (
     <div
-      className="rootnode"
+      className={`rootnode ${isSelected ? "selected" : ""}`}
       tabIndex="0"
       ref={rootRef}
       onDoubleClick={editMode}
-      onKeyDown={(e) => {
-        if ((e.key === "Tab" || e.key === "Enter") && !isEditing) {
-          e.preventDefault();
-          addNode();
-        }
-      }}
     >
-      {isEditing ? (
+      {isEditRoot ? (
         <>
           <div
             ref={inputRef}
@@ -49,12 +67,13 @@ const RootNode = ({ addNode, rootRef, rootNode, setRootNode, rootNodeRef }) => {
               maxWidth: "500px", //最大不能超過500
             }}
             contentEditable="true"
-            //加上這行才不會有Warning: A component is `contentEditable` and contains `children` managed by React....
+            // 加上這行才不會有Warning: A component is `contentEditable` and contains `children` managed by React....
             suppressContentEditableWarning="true"
             onBlur={unEditMode}
             onKeyDown={(e) => {
-              if (e.key === "Enter") {
+              if (e.key === "Enter" || e.key === "Tab") {
                 e.preventDefault();
+                e.stopPropagation();
                 unEditMode(e);
               }
             }}
@@ -70,54 +89,44 @@ const RootNode = ({ addNode, rootRef, rootNode, setRootNode, rootNodeRef }) => {
   );
 };
 
-//節點元件(第二層)
-const Node = ({ node, nodeRef, setNodes, addNode }) => {
+// 節點元件(第二層)
+const Node = ({ node, nodeRef, setNodes, isSelected }) => {
   const [isEditing, setIsEditing] = useState(node.isNew);
   const inputRef = useRef(null);
 
-  // 新增新節點自動聚焦該節點的inputbox
+  // 新增新節點時自動聚焦該節點的inputbox
   useEffect(() => {
     if (isEditing && inputRef.current) {
-      inputRef.current.focus();
       setNodes((prev) =>
         prev.map((item) =>
           item.id === node.id ? { ...item, isNew: false } : item
         )
       );
+      selectText(inputRef.current);
     }
   }, [isEditing, node.id, setNodes]);
 
-  //開啟編輯模式
+  // 開啟編輯模式
   const editMode = () => {
     setIsEditing(true);
   };
-  //關閉編輯模式
+
+  // 關閉編輯模式
   const unEditMode = (e) => {
-    const newName = e.target.innerText;
     setNodes((prev) =>
       prev.map((item) =>
-        item.id === node.id ? { ...item, name: newName } : item
+        item.id === node.id ? { ...item, name: e.target.innerText } : item
       )
-    );
+    ); //找出nodes中符合當前節點ID的元件，並將inputbox輸入的新名稱更新到該節點上
     setIsEditing(false);
-    if (nodeRef.current) {
-      nodeRef.current.focus(); // 關閉編輯模式後將焦點設置回節點
-    }
   };
 
   return (
     <div
-      className="node"
+      className={`node ${isSelected ? "selected" : ""}`}
       tabIndex="0"
       ref={nodeRef}
       onDoubleClick={editMode}
-      onKeyDown={(e) => {
-        if (e.key === "Enter") {
-          e.preventDefault();
-          e.stopPropagation();
-          addNode(node.id);
-        }
-      }}
     >
       {isEditing ? (
         <>
@@ -134,7 +143,7 @@ const Node = ({ node, nodeRef, setNodes, addNode }) => {
             suppressContentEditableWarning="true"
             onBlur={unEditMode}
             onKeyDown={(e) => {
-              if (e.key === "Enter") {
+              if (e.key === "Enter" || e.key === "Tab") {
                 e.preventDefault();
                 e.stopPropagation();
                 unEditMode(e);
@@ -152,41 +161,63 @@ const Node = ({ node, nodeRef, setNodes, addNode }) => {
   );
 };
 
-//心智圖組件
-const MindMap = () => {
-  const [rootNode, setRootNode] = useState({ name: "根節點" });
-  const [nodes, setNodes] = useState([]);
-  const nodeRefs = useRef([]); //宣告一個nodeRefs引用並初始化為空陣列，用來存儲每個節點Dom的引用
-  const rootNodeRef = useRef(null); //用來存儲根節點Dom的引用
-  const pathRef = useRef(null); // 用來存儲線段Dom的引用
+// 心智圖組件
+const MindMap = ({ selectBox, canvasRef }) => {
+  const [rootNode, setRootNode] = useState({ id: uuidv4(), name: "根節點" }); //定義根節點狀態，並設定如何生成id，初始根節點名稱
+  const [isEditRoot, setIsEditRoot] = useState(false); //定義根節點編輯模式狀態，初始為false
+  const [nodes, setNodes] = useState([]); //定義節點們的狀態，用来存儲所有節點，初始為空陣列
+  const [selectedNodes, setSelectedNodes] = useState([]); //定義選中節點們的狀態，初始為空陣列，用來存儲所有被選中的節點id
+  const nodeRefs = useRef([]); //宣告一個引用，初始為空陣列，用來存儲每個引用的節點Dom元素
+  const rootNodeRef = useRef(null); //宣告一個引用，初始為null，用來存儲引用的根節點Dom元素
+  const pathRef = useRef(null); //宣告一個引用，初始為null，用來存儲引用的線段Dom元素
 
-  // 新增節點及每個節點的唯一id，參數id預設為null用來處理根結點新增的部分
+  // 新增節點，參數id預設為null
   const addNode = (id = null) => {
-    const randomColor = `hsl(${Math.floor(Math.random() * 360)}, 90%, 65%)`;
+    const randomColor = `hsl(${Math.floor(Math.random() * 360)}, 90%, 50%)`; //生成一個隨機顏色，用於新節點線段
+    //更新節點陣列狀態，加入新的節點
     setNodes((prev) => {
-      const newNodes = [...prev];
+      const newNodes = [...prev]; //創建一個新的數組，其中包含目前所有節點
+      //創建新節點物件，其中包含所需的屬性
       const newNode = {
         id: uuidv4(),
         name: "節點",
         color: randomColor,
-        isNew: true,
+        isNew: true, //標記為新創建的節點
       };
+      //如果id為 null代表目前選取的是根節點
       if (id === null) {
-        newNodes.push(newNode); // 如果 id 為 null，將新節點添加到數組末尾
-        nodeRefs.current.push(React.createRef()); // 為每個新節點添加一個引用
+        newNodes.push(newNode); //將新節點添加到數組末尾
+        nodeRefs.current.push(React.createRef()); //為每個新節點添加一個引用
       } else {
-        const index = newNodes.findIndex((node) => node.id === id);
+        const index = newNodes.findIndex((node) => node.id === id); //找出陣列中符合id節點之索引
         newNodes.splice(index + 1, 0, newNode); // 在指定id的位置之後插入新節點
         nodeRefs.current.splice(index + 1, 0, React.createRef()); // 為每個新節點添加一個引用
       }
+      setSelectedNodes([newNode.id]); //將原先選擇的節點更新為新節點，轉換焦點
       return newNodes;
     });
   };
 
-  const getRootNodeLoc = () => {
+  // 刪除節點
+  const delNode = useCallback((idArr) => {
+    //更新節點
+    setNodes((prev) => {
+      //去除掉選中的節點；在nodes中篩選出id不包含在idArr中的節點，組成新的節點陣列，代表沒被選中的節點
+      const newNodes = prev.filter((node) => !idArr.includes(node.id));
+      //去除掉選中的節點Dom；在目前引用的節點Dom元素中，篩選出與nodes中對應索引的id不包含在idArr中的引用，組成新的引用陣列，代表沒被選中的節點Dom
+      const newRefs = nodeRefs.current.filter(
+        (item, index) => !idArr.includes(prev[index].id)
+      );
+      nodeRefs.current = newRefs; //更新引用的節點Dom
+      return newNodes;
+    });
+  }, []);
+
+  // 取得根結點svg位置
+  const getRootSvgLoc = () => {
     if (rootNodeRef.current && pathRef.current) {
-      const rootRect = rootNodeRef.current.getBoundingClientRect(); // 獲取根節點的矩形對象
-      const pathRect = pathRef.current.getBoundingClientRect(); // 獲取 SVG 的矩形對象
+      const rootRect = rootNodeRef.current.getBoundingClientRect(); // 獲取根節點的矩形物件
+      const pathRect = pathRef.current.getBoundingClientRect(); // 獲取 SVG 的矩形物件
 
       return {
         x: rootRect.left - pathRect.left + rootRect.width, // 計算path根節點接點的X坐標(相對於g，也就是將g當作視口去計算)
@@ -196,10 +227,11 @@ const MindMap = () => {
     return { x: 0, y: 0 };
   };
 
-  const getNodeLoc = (nodeRef) => {
+  // 取得節點svg位置
+  const getNodeSvgLoc = (nodeRef) => {
     if (nodeRef.current && pathRef.current) {
-      const nodeRect = nodeRef.current.getBoundingClientRect(); // 獲取節點的矩形對象
-      const pathRect = pathRef.current.getBoundingClientRect(); // 獲取 path 的矩形對象
+      const nodeRect = nodeRef.current.getBoundingClientRect(); // 獲取節點的矩形物件
+      const pathRect = pathRef.current.getBoundingClientRect(); // 獲取 path 的矩形物件
       return {
         x: nodeRect.left - pathRect.left, // 計算節點的中心點相對於g的X坐標，也就是將g當作視口去計算
         y: nodeRect.top - pathRect.top + nodeRect.height / 2, // 計算節點的中心點相對於g的Y坐標
@@ -208,10 +240,55 @@ const MindMap = () => {
     return { x: 0, y: 0 };
   };
 
+  // 取得節點canvas位置
+  const getNodeCanvasLoc = (nodeRef, canvasRef) => {
+    if (nodeRef.current && canvasRef.current) {
+      const nodeRect = nodeRef.current.getBoundingClientRect();
+      const canvasRect = canvasRef.current.getBoundingClientRect();
+      return {
+        left: nodeRect.left - canvasRect.left + canvasRef.current.scrollLeft,
+        top: nodeRect.top - canvasRect.top + canvasRef.current.scrollTop,
+        right: nodeRect.right - canvasRect.left + canvasRef.current.scrollLeft,
+        bottom: nodeRect.bottom - canvasRect.top + canvasRef.current.scrollTop,
+      };
+    }
+    return { left: 0, top: 0, right: 0, bottom: 0 };
+  };
+
+  const rootSvgLoc = getRootSvgLoc();
+
+  //判斷節點是否被選中
+  const isNodeSelected = useCallback(
+    (nodeRect) => {
+      if (!selectBox) return false;
+      const selBox = {
+        left: selectBox.left,
+        right: selectBox.left + selectBox.width,
+        top: selectBox.top,
+        bottom: selectBox.top + selectBox.height,
+      };
+
+      const nodeBox = {
+        left: nodeRect.left,
+        right: nodeRect.right,
+        top: nodeRect.top,
+        bottom: nodeRect.bottom,
+      };
+
+      return !(
+        selBox.right < nodeBox.left ||
+        selBox.left > nodeBox.right ||
+        selBox.bottom < nodeBox.top ||
+        selBox.top > nodeBox.bottom
+      );
+    },
+    [selectBox]
+  );
+  //處理節點與根節點的連接線
   useLayoutEffect(() => {
     const updateLocs = () => {
-      const rootNodeLoc = getRootNodeLoc();
-      const nodeLocs = nodeRefs.current.map((ref) => getNodeLoc(ref)); // 獲取每個節點的位置
+      const rootSvgLoc = getRootSvgLoc();
+      const nodeLocs = nodeRefs.current.map((ref) => getNodeSvgLoc(ref)); // 獲取每個節點的位置
       setNodes((prev) =>
         prev.map((node, index) => ({
           ...node,
@@ -219,39 +296,64 @@ const MindMap = () => {
         }))
       );
 
-      setRootNode((prev) => ({ ...prev, position: rootNodeLoc }));
+      setRootNode((prev) => ({ ...prev, position: rootSvgLoc }));
     };
     updateLocs(); // 更新位置
   }, [nodes.length, rootNode.name]); // 當 nodes 的長度發生變化時，重新計算位置
 
-  const rootNodeLoc = getRootNodeLoc();
-
-  // 在 useEffect 中初始滾動至畫布的中心點
-  useEffect(() => {
-    // 處理Tab鍵預設動作
-    window.addEventListener("keydown", (e) => {
-      if (e.key === "Tab") {
-        e.preventDefault();
+  //更新選擇名單
+  useLayoutEffect(() => {
+    if (selectBox) {
+      const selected = [];
+      const rootNodeRect = getNodeCanvasLoc(rootNodeRef, canvasRef);
+      if (isNodeSelected(rootNodeRect)) {
+        selected.push(rootNode.id);
       }
-    });
-    const canvas = document.querySelector(".canvas-wrap");
-    if (canvas) {
-      const { clientWidth, clientHeight, scrollWidth, scrollHeight } = canvas;
-      canvas.scrollTo({
-        left: (scrollWidth - clientWidth) / 2,
-        top: (scrollHeight - clientHeight) / 2,
+      nodes.forEach((node, index) => {
+        const nodeRect = getNodeCanvasLoc(nodeRefs.current[index], canvasRef);
+        if (isNodeSelected(nodeRect)) {
+          selected.push(node.id);
+        }
       });
+      setSelectedNodes(selected);
     }
-  }, []);
+  }, [selectBox, nodes, isNodeSelected, rootNode.id, canvasRef]);
+
+  //處理新增、刪除節點事件監聽
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (
+        (e.key === "Enter" && selectedNodes.length === 1) ||
+        (e.key === "Tab" && selectedNodes[0] === rootNode.id)
+      ) {
+        e.preventDefault();
+        e.stopPropagation();
+        const selectedNodeId = selectedNodes[0];
+        addNode(selectedNodeId === rootNode.id ? null : selectedNodeId);
+      }
+
+      if (e.key === "Delete" && selectedNodes.length > 0) {
+        e.preventDefault();
+        e.stopPropagation();
+        delNode(selectedNodes);
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+    return () => document.removeEventListener("keydown", handleKeyDown); //要移除事件，否則會導致無限循環造成錯誤
+  }, [selectedNodes, isEditRoot, rootNode.id, delNode]);
 
   return (
     <div className="mindmap d-flex align-items-center justify-content-center">
       <RootNode
-        addNode={addNode}
+        isEditRoot={isEditRoot}
+        setIsEditRoot={setIsEditRoot}
         rootRef={rootNodeRef}
         rootNode={rootNode}
         setRootNode={setRootNode}
         rootNodeRef={rootNodeRef}
+        isSelected={selectedNodes.includes(rootNode.id)}
+        setSelectedNodes={setSelectedNodes}
       />
 
       <div className="nodes d-flex justify-content-center flex-column align-items-start">
@@ -261,7 +363,9 @@ const MindMap = () => {
             nodeRef={nodeRefs.current[index]}
             node={nodes[index]}
             setNodes={setNodes}
-            addNode={addNode}
+            delNode={delNode}
+            isSelected={selectedNodes.includes(node.id)}
+            setSelectedNodes={setSelectedNodes}
           />
         ))}
       </div>
@@ -273,20 +377,20 @@ const MindMap = () => {
         ref={pathRef}
       >
         {nodes.map((node, index) => {
-          const nodeLoc = getNodeLoc(nodeRefs.current[index]); // 獲取每個節點的位置
+          const nodeLoc = getNodeSvgLoc(nodeRefs.current[index]); // 獲取每個節點的位置
           return (
             <React.Fragment key={node.id}>
               <path
-                d={`M ${rootNodeLoc.x} ${rootNodeLoc.y} Q ${rootNodeLoc.x} ${nodeLoc.y}, ${nodeLoc.x} ${nodeLoc.y}`}
+                d={`M ${rootSvgLoc.x} ${rootSvgLoc.y} Q ${rootSvgLoc.x} ${nodeLoc.y}, ${nodeLoc.x} ${nodeLoc.y}`}
                 stroke={node.color}
                 fill="none"
                 strokeWidth="3"
               />
-              <circle cx={nodeLoc.x} cy={nodeLoc.y} r="5" fill="blue" />
+              {/* <circle cx={nodeLoc.x} cy={nodeLoc.y} r="5" fill="blue" /> */}
             </React.Fragment>
           );
         })}
-        <circle cx={rootNodeLoc.x} cy={rootNodeLoc.y} r="5" fill="red" />
+        {/* <circle cx={rootSvgLoc.x} cy={rootSvgLoc.y} r="5" fill="red" /> */}
       </svg>
     </div>
   );
--- client/src/pages/WorkArea.js
+++ client/src/pages/WorkArea.js
@@ -1,11 +1,113 @@
-import React from "react";
+// import React from "react";
+// import MindMap from "../MindMap";
+
+// const WorkArea = () => {
+//   return (
+//     <div className="canvas-wrap">
+//       <div className="canvas">
+//         <MindMap />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default WorkArea;
+
+import React, { useState, useRef, useEffect } from "react";
 import MindMap from "../MindMap";
 
 const WorkArea = () => {
+  const [selectBox, setSelectBox] = useState(null); //存儲選擇框位置
+  const selectStart = useRef({ x: 0, y: 0 }); //用來引用並存儲鼠標起始位置，始終不變
+  const canvasRef = useRef(null); //用來引用並存儲畫布Dom
+
+  // 初始滾動至畫布的中心點
+  useEffect(() => {
+    // 處理Tab鍵預設動作
+    window.addEventListener("keydown", (e) => {
+      if (e.key === "Tab") {
+        e.preventDefault();
+      }
+    });
+    if (canvasRef.current) {
+      const { clientWidth, clientHeight, scrollWidth, scrollHeight } =
+        canvasRef.current;
+      canvasRef.current.scrollTo({
+        left: (scrollWidth - clientWidth) / 2,
+        top: (scrollHeight - clientHeight) / 2,
+      });
+    }
+  }, []);
+
+  const handleMouseDown = (e) => {
+    if (e.button !== 0) return; // 只有點擊左鍵觸發
+    const rect = canvasRef.current.getBoundingClientRect(); // 獲取畫布的矩形物件
+    //設定鼠標起點位置
+    selectStart.current = {
+      x: e.clientX + canvasRef.current.scrollLeft - rect.left,
+      y: e.clientY + canvasRef.current.scrollTop - rect.top,
+    };
+    //設定選擇框狀態起點(初始)的尺寸及位置
+    setSelectBox({
+      left: selectStart.current.x,
+      top: selectStart.current.y,
+      width: 0,
+      height: 0,
+    });
+    window.addEventListener("mousemove", handleMouseMove);
+    window.addEventListener("mouseup", handleMouseUp);
+  };
+
+  const handleMouseMove = (e) => {
+    const rect = canvasRef.current.getBoundingClientRect(); // 獲取畫布的矩形物件
+    const x = e.clientX - rect.left + canvasRef.current.scrollLeft; //計算鼠標與畫布的相對x坐標，且隨滾動幅度調整
+    const y = e.clientY - rect.top + canvasRef.current.scrollTop; //計算鼠標與畫布的相對y坐標，且隨滾動幅度調整
+
+    // 檢查鼠標是否靠近視口邊緣，並滾動視口
+    const scrollMargin = 10; // 滾動觸發的邊緣距離
+    const scrollSpeed = 10; // 每次滾動的距離
+
+    if (e.clientX > rect.right - scrollMargin) {
+      canvasRef.current.scrollLeft += scrollSpeed;
+    } else if (e.clientX < rect.left + scrollMargin) {
+      canvasRef.current.scrollLeft -= scrollSpeed;
+    }
+    if (e.clientY > rect.bottom - scrollMargin) {
+      canvasRef.current.scrollTop += scrollSpeed;
+    } else if (e.clientY < rect.top + scrollMargin) {
+      canvasRef.current.scrollTop -= scrollSpeed;
+    }
+
+    // 更新選擇框的位置和尺寸
+    setSelectBox({
+      left: Math.min(x, selectStart.current.x), //不管從哪個方向開始拉，選擇框左測(x座標)始終會是左側，也就是鼠標目前位置與起始位置中比較小的那個值
+      top: Math.min(y, selectStart.current.y),
+      width: Math.abs(x - selectStart.current.x),
+      height: Math.abs(y - selectStart.current.y),
+    });
+  };
+
+  const handleMouseUp = () => {
+    window.removeEventListener("mousemove", handleMouseMove); //移除mousemove事件監聽器，以及該事件監聽器會執行的函式
+    window.removeEventListener("mouseup", handleMouseUp); //移除mouseup事件監聽器，以及該事件監聽器會執行的函式
+    setSelectBox(null); // 放開滑鼠左鍵後隱藏選取框
+  };
+
   return (
-    <div className="canvas-wrap">
+    <div className="canvas-wrap" onMouseDown={handleMouseDown} ref={canvasRef}>
+      {selectBox && (
+        <div
+          className="select-box"
+          style={{
+            left: selectBox.left,
+            top: selectBox.top,
+            width: selectBox.width,
+            height: selectBox.height,
+          }}
+        />
+      )}
       <div className="canvas">
-        <MindMap />
+        <MindMap selectBox={selectBox} canvasRef={canvasRef} />
       </div>
     </div>
   );
